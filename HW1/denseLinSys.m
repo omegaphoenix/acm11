d23b2854b0fafd8cd32d526439a3d07fa6699cc7
Justin Leong <justinleong16@gmail.com>
2017-03-22 15:59:22 -0700

    Finish HW1 Problem 1

---
+++ HW1/denseLinSys.m
@@ -0,0 +1,18 @@
+% Calculate time to solve different size linear systems.
+% Sizes are n = 10, 100, 1000, 2000.
+
+% Clear and close figures
+clear; close all; clc;
+% Size of matrices to solve
+nValues = [10, 100, 1000, 2000];
+for n = nValues
+    S = randMat(n);
+    b = rand(n, 1);
+    % Solve linear system and time it
+    tic;
+    x = S\b;
+    timeElapsed = toc;
+    % Print time it tookk to solve the linear system
+    fprintf('Took  %f time to solve linear system with n=%i\n', timeElapsed,
+    n);
+end
