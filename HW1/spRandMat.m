4ada0d9696900f009891ba8bcc1839d6398b0e0e
Justin Leong <justinleong16@gmail.com>
2017-03-22 16:08:31 -0700

    Answer HW1 Problem 2

---
+++ HW1/spRandMat.m
@@ -0,0 +1,11 @@
+function randSymPosSparseMat = spRandMat(n)
+% RANDMAT Produce a random symmetric, positive definite, sparse n x n matrix
+
+% Construct a random matrix A with non-zero density of 0.05
+density = 0.05;
+randomMat = sprand(n, n, density);
+
+% Return A^T A + I
+randSymPosSparseMat = transpose(randomMat) * randomMat + eye(n);
+
+end
--- HW1/sparseLinSys.m
+++ HW1/sparseLinSys.m
@@ -1,5 +1,5 @@
 % Calculate time to solve different size linear systems constructed using
-% dense random matrices.
+% sparse random matrices..
 % Sizes are n = 10, 100, 1000, 2000.
 
 % Clear and close figures
@@ -7,7 +7,7 @@ clear; close all; clc;
 % Size of matrices to solve
 nValues = [10, 100, 1000, 2000];
 for n = nValues
-    S = randMat(n);
+    S = spRandMat(n);
     b = rand(n, 1);
     % Solve linear system and time it
     tic;
