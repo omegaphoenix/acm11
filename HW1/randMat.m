c057847f3101ce2f55c821ad551af2f2e5c9b925
Justin Leong <justinleong16@gmail.com>
2017-03-21 23:30:09 -0700

    Construct random matrix
    Construct random, symmetric, positive definite, dense n x n matrix.

---
+++ HW1/randMat.m
@@ -0,0 +1,12 @@
+function randSymPosDenseMat = randMat(n)
+% RANDMAT Produce a random symmetric, positive definite, dense n x n matrix
+
+% Construct a random matrix A uniformly distributed on the interval [0, 1]
+randomMat = rand(n, n);
+% Change the interval to [-1, 1]
+randomMat = (randomMat * 2) - 1;
+
+% Return A^T A + I
+randSymPosDenseMat = transpose(randomMat) * randomMat + eye(n);
+
+end
