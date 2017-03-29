469a894e380459c418c300322742b69bc8c502fa
Justin Leong <justinleong16@gmail.com>
2017-03-28 23:15:25 -0700

    Start discrete Laplacian implementation

---
+++ HW1/discreteLaplace.m
@@ -0,0 +1,8 @@
+function L = discreteLaplace(ny, nx, hy, hx)
+% DISCRETELAPLACE Return approximation of Laplace operator
+
+% Side of matrix corresponding to Laplace operator
+nxny = nx * ny;
+% TODO
+% L = spdiags(?, ?, nxny, nxny);
+end
